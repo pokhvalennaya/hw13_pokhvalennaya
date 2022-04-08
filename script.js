56e41a08d7d40e3491b2ac58905fdeaccb2088fd
Lena <elena.pokhvalennaya@gmail.com>
2022-04-08 18:22:35 +0300

    update task

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 fetch("http://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
-  .then((data) => console.log("data", data))
-  .catch((error) => console.error(error));
-
-fetch("http://jsonplaceholder.typicode.com/users/7")
-  .then((response) => response.json())
+  .then((user) => user.find((item) => item.company.name === "Johns Group"))
   .then((data) => console.log("data", data))
   .catch((error) => console.error(error));
